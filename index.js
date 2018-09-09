9b51a98254418e6aafc57dac7d06729929a57649
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-09-09 18:29:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function takeANumber(people, name) {
+   people.push(name)
+  return 'Welcome, ' + name + '. You are number ' + people.length + ' in line.'
+}
+
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return "There is nobody waiting to be served!"
+  }
+  katzDeliLine[0]
+  katzDeliLine.unshift()
+  
+}
+
+function currentLine(current){
+  
+}
