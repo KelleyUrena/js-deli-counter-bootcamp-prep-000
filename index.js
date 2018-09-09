f1a69253ed55476a3b7fe3ce106be959610120c5
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-09-09 18:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(people, name) {
 
 
 function nowServing(katzDeliLine) {
- /* if (katzDeliLine.length === 0) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   }
   
@@ -14,11 +14,11 @@ else {
    katzDeliLine.unshift()
    return 'Currently serving ' + frontOfTheLine + '.'
   }
-*/
+/*
   katzDeliLine[0]
   katzDeliLine.unshift()
   return "There is nobody waiting to be served!"
-  
+  */
 }
 
 function currentLine(people){
