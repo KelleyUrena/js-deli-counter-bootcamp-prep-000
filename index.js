d443aad9b6e6d94f72a8f4b4b7cc1fac599f03f7
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-09-09 18:30:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,33 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length == 0) {
     return "There is nobody waiting to be served!"
   }
-  katzDeliLine[0]
-  katzDeliLine.unshift()
+  
+  else {
+    var frontOfTheLine = katzDeliLine[0]
+    katzDeliLine.unshift()
+    return frontOfTheLine
+  }
+ // katzDeliLine[0]
+ // katzDeliLine.unshift()
+ // return "There is nobody waiting to be served!"
   
 }
 
-function currentLine(current){
+function currentLine(people){
+  if (people.length == 0) {
+    return "The line is currently empty." 
+  }
+  else {
+    var result = 'The line is currently: ' 
+    for(var i = 0; i < people.length ;i++) {
+     result += (i+1)
+     result += '. '
+     result += people[i]
+     result += ', '
+    }
+    result = result.substring(0, result.length - 2)
+    return result 
+    
+  }
   
 }
